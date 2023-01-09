413e546d9bda6e00f33beafc613924e36c860c5d
ndamatta <damatta.natanael@gmail.com>
2023-01-09 13:53:41 -0800

    worked on prep3 assignment

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int random_number = randomGenerator.Next(1, 101);
+
+        int user_number = -1;
+        int tries = 0;
+
+        while (user_number != random_number)
+        {
+            Console.Write("What's the magic number?: ");
+            user_number = int.Parse(Console.ReadLine());
+
+            if (user_number == random_number)
+            {
+                Console.WriteLine("You guessed it!");
+                Console.WriteLine($"It took you {tries} tries");
+                break;
+            }
+            else if (user_number < random_number)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (user_number > random_number)
+            {
+                Console.WriteLine("Lower");
+            }
+
+            tries++;
+        }
+        
     }
 }
