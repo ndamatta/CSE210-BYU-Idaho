5607c5f1273d510459aabfe7dccf4dec7c325dc9
ndamatta <damatta.natanael@gmail.com>
2023-01-11 05:51:28 -0800

    minor changes prep1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,12 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
+        //USER INPUT
         Console.Write("What is your first name?");
         string first_name = Console.ReadLine();
 
         Console.Write("What is your last name?");
         string last_name = Console.ReadLine();
 
+        //OUTPUT
         Console.WriteLine($"Your name is {last_name}, {first_name} {last_name}.");
     }
 }
