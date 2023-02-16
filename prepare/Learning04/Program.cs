70db925b63bb38bf5c42ea48bbd837d465196cee
ndamatta <damatta.natanae@gmail.com>
2023-02-15 20:09:38 -0800

    finished w07 prepare

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+                Assignment assig1 = new Assignment("Pedro Puno", "Multiplication");
+        Console.WriteLine(assig1.GetSummary());
+
+        // Now create the derived class assignments
+        MathAssignment assig2 = new MathAssignment("Joseph Carrick", "Divisions", "7.3", "8-19");
+        Console.WriteLine(assig2.GetSummary());
+        Console.WriteLine(assig2.GetHomeworkList());
+
+        WritingAssignment assig3 = new WritingAssignment("Benson Bennet", "Italian History", "The Causes of World War V");
+        Console.WriteLine(assig3.GetSummary());
+        Console.WriteLine(assig3.GetWritingInformation());
+        
+
     }
 }
