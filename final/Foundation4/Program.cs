95589c494b65d6f34e6d464420b54467ff97fce4
ndamatta <damatta.natanae@gmail.com>
2023-03-30 10:52:20 -0300

    finished foundation 3 and 4

--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation4 World!");
+        List<Activity> activities =  new List<Activity>();
+        activities.Add(new RunningActivity("12 Jan 2021",6.7,30));
+        activities.Add(new CyclingActivity("22 Feb 2015",9.6,30));
+        activities.Add(new Swimming("27 Nov 2022",96,30));
+
+        int index = 1;
+        foreach(Activity activity in activities)
+        {
+            Console.WriteLine($"\nACTIVITY N°{index}:\n{activity.GetSummary()}");
+            index++;
+        }
+        Console.WriteLine();    
     }
 }
